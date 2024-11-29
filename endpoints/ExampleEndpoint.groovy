c9036681eef9990ea14b864a5f1a1f882ff7bb6a
Alex <alexsemenin8@gmail.com>
2024-11-29 10:49:22 +0200

    Example script for Endpoint #4

--- endpoints/ExampleEndpoint.groovy
+++ endpoints/ExampleEndpoint.groovy
@@ -1,2 +1,87 @@
 package endpoints
 
+import com.atlassian.jira.component.ComponentAccessor
+import com.onresolve.scriptrunner.runner.rest.common.CustomEndpointDelegate
+import org.apache.log4j.Logger
+import javax.servlet.http.HttpServletRequest
+import javax.ws.rs.core.MultivaluedMap
+import javax.ws.rs.core.Response
+import com.atlassian.sal.api.user.UserManager
+import com.atlassian.sal.api.user.UserProfile
+
+class ExampleEndpoint extends CustomEndpointDelegate {
+
+    private static final Logger log = Logger.getLogger(this)
+    private static final UserManager userManager = ComponentAccessor.getOSGiComponentInstanceOfType(UserManager)
+
+    ExampleEndpoint() {
+
+        registerRestEndpoint("method1", { MultivaluedMap queryParams, String body, HttpServletRequest request ->
+
+            String extraPath = getAdditionalPath(request)
+            String parameter = queryParams.getFirst("parameter")
+            switch (extraPath) {
+                case "/submethod11":
+                    Response.ok("Success submethod11").build()
+                    break
+                case "/submethod12":
+                    Response.ok("Success submethod12").build()
+                    break
+                default:
+                    Response.ok("Success method1").build()
+            }
+        },  [httpMethod: "GET", "groups": ["rest", "jira-sysadmins"]])
+
+        registerRestEndpoint("method2", { MultivaluedMap queryParams, String body, HttpServletRequest request ->
+
+            String extraPath = getAdditionalPath(request)
+            UserProfile userProfile = userManager.getRemoteUser(request)
+            switch (extraPath) {
+                case "/submethod21":
+                    Response.ok("Success submethod21").build()
+                    break
+                case "/submethod22":
+                    Response.ok("Success submethod22").build()
+                    break
+                default:
+                    Response.ok("Success method2").build()
+            }
+        },  [httpMethod: "POST", "groups": ["rest", "jira-sysadmins"]])
+
+        registerRestEndpoint("method3", { MultivaluedMap queryParams, String body ->
+
+            String extraPath = getAdditionalPath(request)
+            switch (extraPath) {
+                case "/submethod31":
+                    Response.ok("Success submethod31").build()
+                    break
+                case "/submethod32":
+                    Response.ok("Success submethod32").build()
+                    break
+                default:
+                    Response.status(Response.Status.NOT_FOUND).build()
+            }
+        },  [httpMethod: "PUT", "groups": ["rest", "jira-sysadmins"]])
+
+        registerRestEndpoint("method4", { MultivaluedMap queryParams, String body, HttpServletRequest request ->
+
+            String extraPath = getAdditionalPath(request)
+            get
+            switch (extraPath) {
+                case "/submethod41":
+                    Response.ok("Success submethod41").build()
+                    break
+                case "/submethod42":
+                    Response.ok("Success submethod42").build()
+                    break
+                default:
+                    Response.status(Response.Status.INTERNAL_SERVER_ERROR).build()
+            }
+        },  [httpMethod: "GET", "groups": ["rest", "jira-sysadmins"]])
+    }
+
+    @Override
+    run() {
+
+    }
+}
