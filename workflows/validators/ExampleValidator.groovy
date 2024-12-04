dc20913c6509cc065b02749f5ce2e64f0da2084f
Alex <alexsemenin8@gmail.com>
2024-12-04 10:36:35 +0200

    Example script for Validators #9

--- workflows/validators/ExampleValidator.groovy
+++ workflows/validators/ExampleValidator.groovy
@@ -1,2 +1,28 @@
 package workflows.validators
 
+import com.atlassian.jira.issue.Issue
+import com.atlassian.jira.issue.MutableIssue
+import com.opensymphony.workflow.InvalidInputException
+
+class ExampleValidator extends Script {
+
+    @Override
+    run() {
+
+        /**
+         * Instead, use {@code throw new InvalidInputException(...)} to throw the exception directly.
+         */
+        throw new InvalidInputException("")
+
+        /**
+         * The issue object before changes have been applied.
+         * This can be used to determine if any fields have been changed.
+         */
+        Issue originalIssue	= getProperty("originalIssue") as Issue
+
+        /**
+         * The current issue that this field, function, or method applies to.
+         */
+        MutableIssue issue	= getProperty("issue") as MutableIssue
+    }
+}
